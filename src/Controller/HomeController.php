ae73ca1f6ff4d853b1272c62a089a15e82424963
DELORME Loïc <loic.delorme.pro@gmail.com>
2016-03-03 19:08:33 +0100

    Update HomeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,16 +8,28 @@ use Symfony\Component\HttpFoundation\Request;
 class HomeController
 {
     /**
+     * Get categories menus.
+     *
+     * @param Application $app Silex application
+     */
+    public static function getCategoriesMenus(Application $app) {
+        $categoriesMenus = array();
+        $categories = $app['dao.category']->findAll();
+        foreach ($categories as $category) {
+            $subCategories = $app['dao.subCategory']->findByCategory($category->getId());
+            $categoriesMenus[] = array($category, $subCategories);
+        }
+        return $categoriesMenus;
+    }
+
+    /**
      * Home page controller.
      *
      * @param Application $app Silex application
      */
     public function indexAction(Application $app) {
-        $categories = $app['dao.category']->findAll();
-        $subCategories = $app['dao.subCategory']->findAll();
         return $app['twig']->render('index.html.twig', array(
-        	'categories' => $categories,
-        	'subCategories' => $subCategories,
+        	'categoriesMenus' => HomeController::getCategoriesMenus($app),
         	));
     }
 
@@ -28,14 +40,11 @@ class HomeController
      * @param Request $request Incoming request
      * @param Application $app Silex application
      */
-    public function subCategoryItemsAction($id, Request $request, Application $app) {
-        $categories = $app['dao.category']->findAll();
-        $subCategories = $app['dao.subCategory']->findAll();
+    public function subCategoryAction($id, Request $request, Application $app) {
         $subCategory = $app['dao.subCategory']->find($id);
         $items = $app['dao.item']->findBySubCategory($id);
-        return $app['twig']->render('subCategoryItems.html.twig', array(
-            'categories' => $categories,
-            'subCategories' => $subCategories,
+        return $app['twig']->render('subCategory.html.twig', array(
+            'categoriesMenus' => HomeController::getCategoriesMenus($app),
             'subCategory' => $subCategory,
             'items' => $items,
             ));
@@ -49,13 +58,24 @@ class HomeController
      * @param Application $app Silex application
      */
     public function itemAction($id, Request $request, Application $app) {
-        $categories = $app['dao.category']->findAll();
-        $subCategories = $app['dao.subCategory']->findAll();
         $item = $app['dao.item']->find($id);
         return $app['twig']->render('item.html.twig', array(
-            'categories' => $categories,
-            'subCategories' => $subCategories,
+            'categoriesMenus' => HomeController::getCategoriesMenus($app),
             'item' => $item,
+            ));
+    }
+
+    /**
+     * User login controller.
+     *
+     * @param Request $request Incoming request
+     * @param Application $app Silex application
+     */
+    public function loginAction(Request $request, Application $app) {
+        return $app['twig']->render('login.html.twig', array(
+            'categoriesMenus' => HomeController::getCategoriesMenus($app),
+            'error' => $app['security.last_error']($request),
+            'last_username' => $app['session']->get('_security.last_username'),
             ));
     }
 }
