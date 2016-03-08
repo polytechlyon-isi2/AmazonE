bdd45585f8695b2886107e6e437c0a654f8d1e89
Nicolas <nicoloco@laposte.net>
2016-03-08 15:19:17 +0100

    Add Unit Test verifying an anonymous user can show the subcategory list

---
+++ tests/Tests/AnonymousTest.php
@@ -0,0 +1,56 @@
+<?php
+
+
+namespace AmazonE\Tests;
+
+require_once __DIR__.'/../../vendor/autoload.php';
+
+use Silex\WebTestCase;
+use AmazonE\Controller\HomeController;
+
+class AnonymousTest extends WebTestCase{
+
+    /**
+     * @dataProvider provideCategories
+     */
+    public function testSubCategoryExists($subCategory){
+        $client = $this->createClient();
+        $client->request('GET', '/subCategory/'.$subCategory);
+        $this->assertTrue($client->getResponse()->isSuccessful());
+    }
+
+
+    /**
+     * {@inheritDoc}
+     */
+    public function createApplication()
+    {
+        $app = new \Silex\Application();
+
+        require __DIR__.'/../../app/config/dev.php';
+        require __DIR__.'/../../app/app.php';
+        require __DIR__.'/../../app/routes.php';
+
+        // Generate raw exceptions instead of HTML pages if errors occur
+        $app['exception_handler']->disable();
+        // Simulate sessions for testing
+        $app['session.test'] = true;
+        // Enable anonymous access to admin zone
+        $app['security.access_rules'] = array();
+
+        return $app;
+    }
+
+    public function provideCategories(){
+        $app = $this->createApplication();
+        $homeController = new HomeController();
+        $categoriesMenu = $homeController->getCategoriesMenus($app);
+        $subCategories = array();
+        for ($i = 1; $i <= count($categoriesMenu); $i++) {
+            $subCategories[$i] = array($i);
+        }
+        return $subCategories;
+    }
+
+
+}
